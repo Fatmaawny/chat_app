e5ed4698da7075ed0a9257da92b834ac859b0982
fatimahawny <fatimahawny@gmail.com>
2025-08-25 15:37:46 +0300

    add pages' routes

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily:"Ubuntu"
       ),
-        debugShowCheckedModeBanner: true,
+        debugShowCheckedModeBanner: false,
         initialRoute: 'SignInPage');
   }
 }
