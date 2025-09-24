3af75e272bc18e27b1f7a185ab06c000f3b8cc11
fatimahawny <fatimahawny@gmail.com>
2025-09-24 23:13:44 +0300

    chat bubble

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,11 +1,14 @@
 import 'package:chat_app/constants.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/chat_bubble.dart';
+
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
- static String id = 'ChatPage';
+  static String id = 'ChatPage';
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         toolbarHeight: 70,
         automaticallyImplyLeading: false,
@@ -13,13 +16,20 @@ class ChatPage extends StatelessWidget {
         title: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Image.asset(Klogo,
-            height: 62,),
-            SizedBox(width: 10,),
+            Image.asset(Klogo, height: 62),
+            SizedBox(width: 10),
             Text("Chaty app"),
           ],
         ),
         centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ChatBubble(text: "Hi how are you",),
+          );
+        },
       ),
     );
   }
---
+++ lib/widgets/chat_bubble.dart
@@ -0,0 +1,26 @@
+import 'package:chat_app/constants.dart';
+import 'package:flutter/material.dart';
+class ChatBubble extends StatelessWidget {
+  const ChatBubble({super.key, required this.text});
+final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        decoration: BoxDecoration(
+          color: KprimaryColor,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0,vertical: 17),
+          child: Text(text,style: TextStyle(fontSize: 20),),
+        ),
+      ),
+    );
+  }
+}
