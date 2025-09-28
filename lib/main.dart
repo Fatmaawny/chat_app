3e9da8a6f08cbf7b2a817636a7a3abc9a0350dca
fatimahawny <fatimahawny@gmail.com>
2025-09-28 23:20:24 +0300

    make a collection and connect it

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,11 +1,15 @@
 import 'package:chat_app/constants.dart';
 import 'package:flutter/material.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import '../widgets/chat_bubble.dart';
 
 class ChatPage extends StatelessWidget {
-  const ChatPage({super.key});
+  CollectionReference messages = FirebaseFirestore.instance.collection(
+    'messages',
+  );
   static String id = 'ChatPage';
+  TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +22,14 @@ class ChatPage extends StatelessWidget {
           children: [
             Image.asset(Klogo, height: 62),
             SizedBox(width: 10),
-            Text("Chaty app",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 25),),
+            Text(
+              "Chaty app",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            ),
           ],
         ),
         centerTitle: true,
@@ -39,21 +50,33 @@ class ChatPage extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
+              controller: controller,
+              onSubmitted: (data) async {
+                await messages.add({'text': data});
+                controller.clear();
+              },
               decoration: InputDecoration(
                 hintText: "Send a message",
-                suffixIcon: Icon(Icons.send,color: KprimaryColor,),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.send, color: KprimaryColor),
+                  onPressed: () async {
+                    if (controller.text.isNotEmpty) {
+                      await messages.add({'text': controller.text});
+                      controller.clear();
+                    }
+                  },
+                ),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(20),
-                  borderSide: BorderSide(color: KprimaryColor,width: 2)
+                  borderSide: BorderSide(color: KprimaryColor, width: 2),
                 ),
                 focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide(color: KprimaryColor,width: 2)
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(color: KprimaryColor, width: 2),
                 ),
-
               ),
             ),
-          )
+          ),
         ],
       ),
     );
