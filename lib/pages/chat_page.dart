3684fe659ea876a227acf684cf7f74e1df719875
fatimahawny <fatimahawny@gmail.com>
2025-09-29 14:31:09 +0300

    real time messages

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -2,97 +2,110 @@ import 'package:chat_app/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import '../models/message_model.dart';
 import '../widgets/chat_bubble.dart';
 
-class ChatPage extends StatelessWidget {
-  CollectionReference messages = FirebaseFirestore.instance.collection(
-    'messages',
-  );
+class ChatPage extends StatefulWidget {
   static String id = 'ChatPage';
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  CollectionReference messages = FirebaseFirestore.instance.collection('messages',);
+
   TextEditingController controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<QuerySnapshot>(
-        future: messages.get(),
 
-        builder: (context, snapshot){
-          for(int i=0;i<snapshot.data!.docs.length;i++){
-            print(snapshot.data!.docs[i]['text']);
-          }
-          if(snapshot.hasData){
-            return Scaffold(
-              appBar: AppBar(
-                toolbarHeight: 70,
-                automaticallyImplyLeading: false,
-                backgroundColor: KprimaryColor,
-                title: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.asset(Klogo, height: 62),
-                    SizedBox(width: 10),
-                    Text(
-                      "Chaty app",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25,
-                      ),
-                    ),
-                  ],
-                ),
-                centerTitle: true,
-              ),
-              body: Column(
+    return StreamBuilder<QuerySnapshot>(
+
+      stream: messages.snapshots(),
+      builder: (context, snapshot) {
+
+        List<MessageModel> messagesList = [];//ليست لسه فاضية
+
+        for (int i = 0; i < snapshot.data!.docs.length; i++) {
+          messagesList.add(MessageModel.fromjson(snapshot.data!.docs[i]));
+        }
+
+        if (snapshot.hasData) {
+          return Scaffold(
+            appBar: AppBar(
+              toolbarHeight: 70,
+              automaticallyImplyLeading: false,
+              backgroundColor: KprimaryColor,
+              title: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Expanded(
-                    flex: 1,
-                    child: ListView.builder(
-                      itemCount: snapshot.data!.docs.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: ChatBubble(text: snapshot.data!.docs[index]['text']),
-                        );
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextField(
-                      controller: controller,
-                      onSubmitted: (data) async {
-                        await messages.add({'text': data});
-                        controller.clear();
-                      },
-                      decoration: InputDecoration(
-                        hintText: "Send a message",
-                        suffixIcon: IconButton(
-                          icon: Icon(Icons.send, color: KprimaryColor),
-                          onPressed: () async {
-                            if (controller.text.isNotEmpty) {
-                              await messages.add({'text': controller.text});
-                              controller.clear();
-                            }
-                          },
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: BorderSide(color: KprimaryColor, width: 2),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: BorderSide(color: KprimaryColor, width: 2),
-                        ),
-                      ),
+                  Image.asset(Klogo, height: 62),
+                  SizedBox(width: 10),
+                  Text(
+                    "Chaty app",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
                     ),
                   ),
                 ],
               ),
-            );
-          }
-          else
-            return Text("waiting...");
-
-        });
+              centerTitle: true,
+            ),
+            body: Column(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: ListView.builder(
+                    itemCount: messagesList.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: ChatBubble(
+                          text: messagesList[index].message,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: controller,
+                    onSubmitted: (data) async {
+                      await messages.add({'text': controller.text});
+                      controller.clear();
+                    },
+                    decoration: InputDecoration(
+                      hintText: "Send a message",
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.send, color: KprimaryColor),
+                        onPressed: () async {
+                          if (controller.text.isNotEmpty) {
+                            await messages.add({'text': controller.text});
+                            controller.clear();
+                          }
+                        },
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(color: KprimaryColor, width: 2),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(color: KprimaryColor, width: 2),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        } else
+          return Text("waiting...");
+      },
+    );
   }
 }
